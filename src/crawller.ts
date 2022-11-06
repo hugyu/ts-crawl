08ffd4e47cd1b247fef7dbd97f54659fa288d925
hgy <1500586026@qq.com>
2022-11-06 19:30:30 +0800

    将结果写入course.json

--- src/crawller.ts
+++ src/crawller.ts
@@ -1,9 +1,21 @@
+// 引入核心模块 写入json文件
+import fs from 'fs';
+import path from 'path';
 import superagent from "superagent";
 // 对获取到的html文档进行操作
 import cheerio from "cheerio";
 interface CourseItem {
     title: string,
     count:number
+}
+
+interface CourseResult {
+  time: number,
+  data: CourseItem[]
+}
+
+interface JsonContent {
+  [propName:number]:CourseItem[],
 }
 class Crawller {
   private secret = "secretKey";
@@ -24,17 +36,39 @@ class Crawller {
             time: (new Date()).getTime(),
             data: courseInfos
         }
-        console.log(result);
+      console.log(result);
+      return result;
         
   }
+  
+  generateJsonContent(courseResult: CourseResult) {
+    const filePath = path.resolve(__dirname, '../data/course.json')
+    let fileContent:JsonContent={}
+    if (fs.existsSync(filePath)) {
+      fileContent = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+      
+    }
+    fileContent[courseResult.time] = courseResult.data
+    return fileContent
+  }
+
   // 获取原始的html文件
   async getRawHtml() {
     // 发送get请求
     const result = await superagent.get(this.url);
-    this.getCourseInfo(result.text);
+    return result.text
+  }
+
+  async initSpiderProgress() {
+    const filePath = path.resolve(__dirname, '../data/course.json')
+    const html = await this.getRawHtml()
+    const courseResult = this.getCourseInfo(html);
+    const fileContent = this.generateJsonContent(courseResult)
+    fs.writeFileSync(filePath,JSON.stringify(fileContent))
+    
   }
   constructor() {
-    this.getRawHtml();
+    this.initSpiderProgress();
   }
 }
 const crawller = new Crawller();
